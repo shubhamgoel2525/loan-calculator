8f722f433b7d62286c7e3576a6619ec25cad3fa4
Shubham <shubhamgoel2525@gmail.com>
2021-03-12 19:48:12 +0530

    Fixes

--- src/components/LoanView/LoanView.js
+++ src/components/LoanView/LoanView.js
@@ -6,11 +6,11 @@ const LoanView = (props) => {
         <div className={styles.container}>
             <div className={styles.mainCard}>
                 <h4>Loan Amount</h4>
-                <h1>{props.monthlyPay.toFixed(3)}</h1>
+                <h1>{parseFloat(props.monthlyPay).toFixed(3)}</h1>
             </div>
             <div className={styles.bottomCard}>
-                <div className={styles.row}><p>Principal</p> <p>{props.principal.toFixed(3)}</p></div>
-                <div className={styles.row}><p>Interest</p> <p>{props.interest.toFixed(3)}</p></div>
+                <div className={styles.row}><p>Principal</p> <p>{parseFloat(props.principal).toFixed(3)}</p></div>
+                <div className={styles.row}><p>Interest</p> <p>{parseFloat(props.interest).toFixed(3)}</p></div>
             </div>
         </div>
     );
